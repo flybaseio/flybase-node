d18bd463471e8c995baa00b1963f6fd1bceaae22
roger@datamcfly.com <Roger Stringer>
2018-07-12 19:28:38 +0000

    1.8.6

--- flybase/index.js
+++ flybase/index.js
@@ -230,7 +230,7 @@ Flybase.prototype.toString = function(){
 Flybase.prototype.startWebSocket = function ( channel ){
 	var _this = this;
 	this.socket = require('socket.io-client')(
-		'http://push.flybase.io',{
+		'https://push.flybase.io',{
 		forceNew: true,
 		'pingTimeout': 7000,
 		'pingInterval': 3000
